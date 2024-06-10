992cb47e482a241ee121a8064e5f031e26f2fdd2
Dbonne88 <tcatastros@gmail.com>
2024-06-10 12:20:03 -0500

    Es la segunda modificacion dentro de Master

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("Primer repositorio");
 console.log("Segundo cambio repositorio");
-console.log("Este es el primer cambio en la rama1");
+console.log("Este es el primer cambio en la MASTER");
+console.log("Este es la segunda modificacion que se hace dentro de la rama MASTER");
